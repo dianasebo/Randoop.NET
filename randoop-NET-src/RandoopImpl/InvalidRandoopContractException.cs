cfa4ce6b3ed6bd5dafea53cb0bd71d8f19c723ef
Diana <sebodiana@gmail.com>
2020-05-24 19:26:05 +0300

    Moved InvalidRandoopContractException from RandoopContracts

---
+++ randoop-NET-src/RandoopImpl/InvalidRandoopContractException.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Randoop
+{
+    public class InvalidRandoopContractException : Exception
+    {
+        public InvalidRandoopContractException() { }
+
+        public InvalidRandoopContractException(string message) : base(message) { }
+
+        public InvalidRandoopContractException(string message, Exception innerException) : base(message, innerException) { }
+    }
+}
